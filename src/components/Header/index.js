91dbb582969b0ae814bdee0ee41b7c382cb0f615
Warro <olli.warro@gmail.com>
2019-11-16 18:15:41 +0200

    fixed header height

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { IoIosMenu } from 'react-icons/io'
 const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  height: 56px;
+  min-height: 56px;
   padding: 0 15px 0 15px;
   box-shadow: 0 1px 3px 0 rgba(0,0,0,.2);
   width: 100%;
