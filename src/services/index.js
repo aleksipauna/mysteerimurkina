75fcf8e59c4f9fd96d36be44abe70b22a1594d15
coldDitch <juuso.kylmaoja@aalto.fi>
2019-11-17 00:23:54 +0200

    ignoring stupid food

--- src/services/index.js
+++ src/services/index.js
@@ -3,12 +3,26 @@ const healthy = ['Kasvisruoat','Kasvislisäkkeet','Kasvis']
 const convenient = ['Mukavuudenhaluiset','Helppo','Puolivalmisteita sisältävä','Lyhyt aktiivinen valmistusaika',]
 const aware = ['Kasvisruoat','Kasvislisäkkeet','Kasvis']
 const exotic = ['Makuja maailmalta']
+const ignore = ['Makeat leivonnaiset','Juomat','Kuumat juomat ','Pikkujoulut']
 var seed = 2;
 
 function my_random() {
     var x = Math.sin(seed++) * 10000;
     return x - Math.floor(x);
 }
+
+const remove = function(recipes, words) {
+  let res = recipes.filter(el => {
+      let flag = true
+      words.forEach(f => {
+        if (el.categories.indexOf(f) >= 0) {
+        flag = false
+        }
+      })
+      return flag
+    })
+  return res
+} 
 
 const filter_recipes = function(recipes, words) {
   let res = recipes.filter(el => {
@@ -24,7 +38,17 @@ const filter_recipes = function(recipes, words) {
 } 
 
 export const getPrediction = function(foodTypes, allergens, diets) {
-  let recipes = json;
+  let recipes = remove(json, ignore);
+  console.log(recipes)
+  diets.forEach(el => {
+    switch(el) {
+      case 'vegan':
+          recipes = filter_recipes(recipes, healthy)
+      case 'vegetarian':
+          recipes = filter_recipes(recipes, healthy)
+    }
+
+  })
   foodTypes.forEach(el => {
     switch(el) {
         case 'healthy':
