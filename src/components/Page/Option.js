e53156d03c06504e01de78d964611d81eb8ca073
Warro <olli.warro@gmail.com>
2019-11-16 17:49:17 +0200

    stylerino

--- src/components/Page/Option.js
+++ src/components/Page/Option.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import styled from 'styled-components';
-import { FaArrowRight } from 'react-icons/fa';
-import { COLOR_ORANGE, COLOR_GRAY } from '../constants'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const OptionHolder = styled.div`
     margin: 1em;
@@ -13,53 +11,59 @@ const OptionHolder = styled.div`
     cursor: pointer;
     box-shadow: 0 .25rem .25rem rgba(0,0,0,0.2),
     0 0 1rem rgba(0,0,0,0.2);
-    background-image: url("${(props) => props.src}");
-    transition: transform .2s;
+    background-image: url("${props => props.src}");
+    transition: background-size .4s;
     :hover {
-        transform: scale(1.03); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+        background-size: auto 105%;
     }
 `;
 
 const OptionHeader = styled.header`
   color: black;
-  font-family: 'Public Sans', sans-serif;
+  font-family: "Public Sans", sans-serif;
   font-size: 16px;
   padding: 0.5em 0.5em 0em 0.5em;
 `;
 
 const OptionText = styled.p`
   color: black;
-  font-family: 'Public Sans', sans-serif;
+  font-family: "Public Sans", sans-serif;
   font-size: 13px;
   padding: 0.5em;
 `;
 const ContentBox = styled.div`
-    background-color: #FAFAFA ;
-    height: 5em;
-    width: 100%;
+  background-color: #fafafa;
+  height: 5em;
+  width: 100%;
+  transition: filter 0.4s;
+  filter: ${({ hover }) => (hover ? "brightness(90%);" : "none")};
 `;
 
 const OptionContent = styled.div`
-    width: 100%;
-    height: 100%;
-    display:flex;
-    justify-content: left;
-    align-items: flex-end;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: left;
+  align-items: flex-end;
 `;
 
-const Option = (props) => {
-    return (
-        <OptionHolder src={props.pic} onClick={props.onClick}>
-            <OptionContent>
-                <ContentBox>
-                    <OptionHeader>
-                        {props.title} 
-                    </OptionHeader>
-                    <OptionText>{props.content}</OptionText>
-                </ContentBox>
-            </OptionContent>
-        </OptionHolder>
-    )
-}
+const Option = props => {
+  const [hover, setHover] = useState(false);
+  return (
+    <OptionHolder
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      src={props.pic}
+      onClick={props.onClick}
+    >
+      <OptionContent>
+        <ContentBox hover={hover}>
+          <OptionHeader>{props.title}</OptionHeader>
+          <OptionText>{props.content}</OptionText>
+        </ContentBox>
+      </OptionContent>
+    </OptionHolder>
+  );
+};
 
 export default Option;
