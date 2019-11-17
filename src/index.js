2d9615b1e047ffa440bffb698f3ba461ea875d6f
Warro <olli.warro@gmail.com>
2019-11-17 20:16:42 +0200

    Added GA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Main from './components/Main'
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+import Main from "./components/Main";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+
+ReactGA.initialize("UA-141944798-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<Main />, document.getElementById("root"));
